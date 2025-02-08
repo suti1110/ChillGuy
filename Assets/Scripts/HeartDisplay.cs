a33d3eba98448d3bae44669a48211e074d5a783a
gamzagratang <24sunrin117@sunrint.hs.kr>
2025-02-09 04:05:15 +0900

    버그수정

--- Assets/Scripts/HeartDisplay.cs
+++ Assets/Scripts/HeartDisplay.cs
@@ -6,6 +6,7 @@ public class HeartDisplay : MonoBehaviour
     public GetDamage getDamageScript;  // GetDamage 스크립트를 참조
     public GameObject heartPrefab;     // 하트 프리팹
     public Transform heartContainer;   // 하트를 배치할 위치 (보통 UI에 배치)
+    GameObject[] Hearts = new GameObject[3];
 
     private void Start()
     {
@@ -22,11 +23,20 @@ public class HeartDisplay : MonoBehaviour
 
     public void UpdateHeartDisplay()
     {
+        if (Hearts.Length != 0)
+        {
+            for (int i = 0; i < Hearts.Length; i++)
+            {
+                Destroy(Hearts[i]);
+                // 하트 위치를 조정하거나 추가적인 설정을 할 수 있습니다.
+            }
+        }
+
         // 현재 Hearth 수에 맞는 하트를 추가
         for (int i = 0; i < getDamageScript.health; i++)
         {
-            GameObject heart = Instantiate(heartPrefab, heartContainer.position + new Vector3(i * 70, 0), Quaternion.identity);
-            heart.transform.parent = transform;
+            Hearts[i] = Instantiate(heartPrefab, heartContainer.position + new Vector3(i * 70, 0), Quaternion.identity);
+            Hearts[i].transform.parent = transform;
             // 하트 위치를 조정하거나 추가적인 설정을 할 수 있습니다.
         }
     }
